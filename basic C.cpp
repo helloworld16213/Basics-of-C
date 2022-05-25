e9e6a0c8a024cc71c298bc5ce3740c9cbf0b4fff
helloworld16213 <97744022+helloworld16213@users.noreply.github.com>
2022-05-25 23:23:16 +0530

    Add files via upload

---
+++ basic C.cpp
@@ -0,0 +1,15 @@
+/* Author - Lakshita Dubey 
+Date - 25th may 2022
+Checking basic working in C */
+
+#include "stdio.h"
+
+int main()
+{
+	const int a = 10;
+	printf("%d", a);
+	
+	int b = 200;
+	b = 50;
+	printf("%d", b);
+}
